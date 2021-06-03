16bded11700fa0260772b60ea4185e60159eb830
MaximeMalatier <70962311+MaximeMalatier@users.noreply.github.com>
2021-06-03 15:47:44 +0200

    Delete ConnectSound directory

--- ConnectSound/configPDO.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-
-$bdd = new PDO('mysql:host=0y34u.myd.infomaniak.com;dbname=0y34u_bdd; charset=utf8' ,'0y34u_bdd','Bdd170902',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-?>
